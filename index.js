1367ad594b627838d2e298d22c878134ef41924f
Grace Shih <vocalise@gmail.com>
2020-01-03 06:37:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function fetchBooks() {
-
+fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response) {return response.json()})
+  .then(function(json) { return renderBooks(json)});
+  
 }
 
 function renderBooks(json) {
