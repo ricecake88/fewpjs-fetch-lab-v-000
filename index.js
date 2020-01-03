7b97cab071bda61d7d5793e54b8bad2279deeffd
Grace Shih <vocalise@gmail.com>
2020-01-03 06:44:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   fetch('https://anapioficeandfire.com/api/books')
-    .then(function(response) {return response.json()})
+    .then(function(response) {
+      return response.json()
+    })[]
   
 }
 
