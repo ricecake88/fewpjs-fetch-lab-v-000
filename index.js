4cc56efc1485268fa2e9415e82ba978594a37932
Grace Shih <vocalise@gmail.com>
2020-01-03 06:50:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ fetch('https://anapioficeandfire.com/api/books')
 function renderBooks(json) {
   const main = document.querySelector('main')
   json.forEach(book => {
-    console.log(book)
     const h2 = document.createElement('h2')
     h2.innerHTML = `<h2>${book.name}</h2>`
     main.appendChild(h2)
